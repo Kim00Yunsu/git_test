1ef6541d69a5f9a78994a1847191b1be882682ff
Kimyunsu <yunsu3375@naver.com>
2024-10-03 00:43:37 +0900

    [Add Feature] se2_test.java 추가

---
+++ src/main/java/org/example/se2_test.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class se2_test {
+    public void printSomething(String name) {
+        System.out.println("Hello World"+name);
+    }
+
+    public static void main(String[] args) {
+        se2_test me = new se2_test();
+        me.printSomething("Sourcecode");
+    }
+
+
+}
